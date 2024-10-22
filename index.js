b2f763bc5e9b058992605d9700def9f105a3ec1e
Bolormaa222 <bolormaaenkhbat222@gmail.com>
2024-10-23 03:12:12 +0800

    ok

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/cron", (request, response) => {
     response.send(status);
 });
 
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.log("Server Listening on PORT:", 8080);
 });
 
@@ -34,20 +34,33 @@ app.listen(8080, () => {
 
 cron.schedule('* * * * *', async () => {
     console.log('running every minute 1, 2, 4 and 5');
-    const resultcv = await fetch("https://cvback-yj18.onrender.com/",{
-      method:"GET"
-    });
-    let rescv = await resultcv.json()
-    console.log("res ", rescv)
-    const resultupload = await fetch("https://upload-wps4.onrender.com/remove/video",{
-      method:"GET"
-    });
-    let resupload = await resultupload.json()
-    console.log("res upload ", resupload)
-    const resultclient = await fetch("https://elearnclient.onrender.com/api/auth/me",{
-      method:"GET"
-    });
-    let resclient = await resultclient.json()
-    console.log("res client ", resclient)
+    try{
+      const resultcv = await fetch("https://cvback-yj18.onrender.com/api/v1/test",{
+        method:"GET"
+      });
+      let rescv = await resultcv.json()
+      console.log("res ", rescv)
+    }catch(err){
+      console.log("res err ", err)
+    }
+    try{
+      const resultupload = await fetch("https://upload-wps4.onrender.com/",{
+        method:"GET"
+      });
+      let resupload = await resultupload.json()
+      console.log("res upload ", resupload)
+    }catch(err){
+      console.log("res upload err ", err);
+    }
+    try{
+      const resultclient = await fetch("https://elearnclient.onrender.com/api/auth/me",{
+        method:"GET"
+      });
+      let resclient = await resultclient.json()
+      console.log("res client ", resclient)
+    }catch(err){
+      console.log("res client err ", err)
+    }
+    
     
 });
