ee21729eab34e8b7e448d49be17e7893f721b7c4
Bolormaa222 <bolormaaenkhbat222@gmail.com>
2024-10-22 15:45:13 +0800

    commit

--- index.js
+++ index.js
@@ -34,9 +34,14 @@ app.listen(8080, () => {
 
 cron.schedule('* * * * *', async () => {
     console.log('running every minute 1, 2, 4 and 5');
-    const result = await fetch("https://cvback-yj18.onrender.com/",{
+    const resultcv = await fetch("https://cvback-yj18.onrender.com/",{
       method:"GET"
     });
-    let res = await result.json()
-    console.log("res ", res)
-  });
+    let rescv = await resultcv.json()
+    console.log("res ", rescv)
+    const resultupload = await fetch("https://upload-wps4.onrender.com/remove/video",{
+      method:"GET"
+    });
+    let resupload = await resultupload.json()
+    console.log("res upload ", resupload)
+});
